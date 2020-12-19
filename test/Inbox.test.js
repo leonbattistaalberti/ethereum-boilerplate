97b641563789d4fa3b5bd7ed5ff2d95d84c15ebd
leon_battist_alberti <leonbattista189@gmail.com>
2020-12-19 20:51:43 +0000

    Deploy contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,15 +4,22 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 // Create web3 instance
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
+
+  // Deploy Inbox contract using a Ganache account
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi"] })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(accounts);
+    console.log(inbox);
   });
 });
